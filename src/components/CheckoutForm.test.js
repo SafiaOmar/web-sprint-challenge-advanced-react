fc272c710820550b13750b163661539c8447d932
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-12-03 11:52:42 -0500

    still testing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,35 +1,45 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
-const firstName = "Sofia";
-const lastName = "Omar";
-const address = "1111 Code St";
-const city = "Columbus";
-const state = "Ohio";
-const zipCode = "43220";
+// Write up the two tests here and make sure they are testing what the title shows
 
-test( "form header renders", () => {
-	render( <CheckoutForm/> );
+test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  const header = screen.getByText("Checkout");
+  expect(header).toHaveTextContent(/checkout/i);
+});
 
-	expect( screen.getByText( /Checkout Form/i ) );
-} );
+test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+  render(<CheckoutForm />);
 
-test( "form shows success message on submit with form details", async () => {
-	render( <CheckoutForm/> );
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  const addressInput = screen.getByLabelText(/address/i);
+  const cityInput = screen.getByLabelText(/city/i);
+  const stateInput = screen.getByLabelText(/state/i);
+  const zipInput = screen.getByLabelText(/zip/i);
+  const submitButton = screen.getByRole("button", { name: /checkout/i });
 
-	const firstInput = screen.getByLabelText( /first/i );
-	const lastInput = screen.getByLabelText( /last/i );
-	const addressInput = screen.getByLabelText( /address/i );
-	const cityInput = screen.getByLabelText( /city/i );
-	const stateInput = screen.getByLabelText( /state/i );
-	const zipInput = screen.getByLabelText( /zip/i );
-
-	await userEvent.type( firstInput, firstName );
-	await userEvent.type( lastInput, lastName );
-	await userEvent.type( addressInput, address );
-	await userEvent.type( cityInput, city );
-	await userEvent.type( stateInput, state );
-	await userEvent.type( zipInput, zipCode );
-}
+  fireEvent.change(firstNameInput, {
+    target: { name: "firstName", value: "Sofia" },
+  });
+  fireEvent.change(lastNameInput, {
+    target: { name: "lastName", value: "Omar" },
+  });
+  fireEvent.change(addressInput, {
+    target: { name: "address", value: "123 Code St" },
+  });
+  fireEvent.change(cityInput, {
+    target: { name: "city", value: "Columbus" },
+  });
+  fireEvent.change(stateInput, {
+    target: { name: "state", value: "Ohio" },
+  });
+  fireEvent.change(zipInput, {
+    target: { name: "zip", value: "43220" },
+  });
+  fireEvent.click(submitButton);
+});
